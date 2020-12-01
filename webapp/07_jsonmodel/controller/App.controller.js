a5c295b293730393f3f37d1d37edb3b73b543bf5
lovy <lovy.zhu@sap.com>
2020-12-01 18:09:32 +0800

    08 -> i18n

---
+++ webapp/07_jsonmodel/controller/App.controller.js
@@ -0,0 +1,25 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel"
+], function (Controller, MessageToast, JSONModel, ResourceModel) {
+    "use strict";
+
+    return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
+        onInit: function () {
+            // set data model on view
+            var oData = {
+                recipient: {
+                    name: "World"
+                }
+            };
+            var oModel = new JSONModel(oData);
+            this.getView().setModel(oModel);
+        },
+
+        onShowHello: function () {
+            MessageToast.show("Hello World");
+        }
+    });
+});
